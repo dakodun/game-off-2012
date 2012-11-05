9fe5cfd67cf3b6b33d7a7a091f04642add6b0e60
dakodun <jinto@chikin.net>
2012-11-05 11:54:16 +0000

    intial commit; a game object contains all logic and data for our game

---
+++ parts/game.js
@@ -0,0 +1,49 @@
+// Game Class...
+// a game object contains all the logic and data of our game
+function Game() {
+	this.mCanvas = null;
+	this.mContext = null;
+	
+	this.mQuit = false;
+};
+
+// initialises the game object
+Game.prototype.SetUp = function() {
+	this.mCanvas = document.getElementById("canvas"); // get the canvas element handle by id from the html file
+	this.mContext = this.mCanvas.getContext("2d"); // get a 2d context handle from the canvas
+};
+
+// cleans up the game object
+Game.prototype.TearDown = function() {
+	
+};
+
+Game.prototype.Run = function() {
+	// while the app is to run
+	do {
+		this.Input(); // perform input handling
+		this.Process(); // process the game
+		this.Render(); // render the results
+	} while(!mQuit);
+}
+
+Game.prototype.Input = function() {
+	mCurrScene.Input(); // perform input for the current scene
+}
+
+Game.prototype.Process = function() {
+	mCurrScene.Process(); // process the current scene
+}
+
+Game.prototype.Render = function() {
+	mCurrScene.Render(); // render the current scene
+}
+// ...End
+
+
+// main Namespace...
+var nmain = new function() {
+	this.game = new Game(); // our game object
+}
+// ...End
+
