93ed5c59b78fdae9e88756d721edbfba0c953146
dakodun <jinto@chikin.net>
2012-11-06 13:17:27 +0000

    changed main function to use new game loop (see game.js)

--- parts/main.js
+++ parts/main.js
@@ -7,9 +7,11 @@ var nmanagers = new function() {
 
 function main() {
 	try {
-		nmain.game.SetUp();
-		nmain.game.Run();
-		nmain.game.TearDown();
+		nmain.game.SetUp(); // initialise the game
+		
+		// run the game loop as fast as the browser will allow
+		// note that timing is handled elsewhere (within the Game Run() function)
+		nmain.game.mGameLoop = setInterval(function() {nmain.game.Run()}, 0);
 	} catch(e) {
 		alert(e.What());
 	}
