7027cbc0a5c2ede83d7ca3672dff6e4b03beb751
dakodun <jinto@chikin.net>
2012-11-07 16:30:09 +0000

    added reference to a texture store

--- parts/initscene.js
+++ parts/initscene.js
@@ -4,17 +4,25 @@ function InitScene() {
 }
 
 // returns the type of this object for validity checking
-IVec2.prototype.Type = function() {
+InitScene.prototype.Type = function() {
 	return "InitScene";
 };
 
 // 
-IVec2.prototype.Persistent = function() {
+InitScene.prototype.Persistent = function() {
 	return persist;
 };
 
 InitScene.prototype.SetUp = function() {
+	// var tex = new Texture();
+	// tex.LoadFromFile("./res/vis/test.png");
 	
+	try {
+		var t = (nmgrs.resMan.mTexStore.AddResource(new Texture(), "test")).mRes;
+		t.LoadFromFile("./res/vis/test.png");
+	} catch(e) {
+		alert(e.What());
+	}
 }
 
 InitScene.prototype.TearDown = function() {
@@ -30,7 +38,13 @@ InitScene.prototype.Process = function() {
 }
 
 InitScene.prototype.Render = function() {
+	// var tex = new Texture();
+	// tex.LoadFromFile("./res/vis/test.png");
 	
+	var tex = (nmgrs.resMan.mTexStore.GetResource("test")).mRes;
+	
+	// nmain.game.mContext.fillText("Hello", 50, 50);
+	nmain.game.mContext.drawImage(tex.mImg, 0, 0);
 }
 // ...End
 
