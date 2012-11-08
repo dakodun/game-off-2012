57bf3dcd336ffe5db0a43c84a037368b50f0345b
dakodun <jinto@chikin.net>
2012-11-08 12:48:00 +0000

    added reference to resource loader; added semi-colon to interval function

--- parts/main.js
+++ parts/main.js
@@ -2,6 +2,7 @@
 var nmgrs = new function() {
 	this.sceneMan = new SceneManager();
 	this.resMan = new ResourceManager();
+	this.resLoad = new ResourceLoader();
 }
 // ...End
 
@@ -13,7 +14,7 @@ function main() {
 		// run the game loop as fast as the browser will allow
 		// note that timing is handled elsewhere (within the Game Run() function)
 		nmain.game.mTimer.Reset();
-		nmain.game.mGameLoop = setInterval(function() {nmain.game.Run()}, 0);
+		nmain.game.mGameLoop = setInterval(function() {nmain.game.Run();}, 0);
 	} catch(e) {
 		alert(e.What());
 	}
