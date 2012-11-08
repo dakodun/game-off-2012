69333ea97ce74988ff21ddc9dc7c172f8e92c3fa
dakodun <jinto@chikin.net>
2012-11-08 21:08:49 +0000

    added a function to mimic a copy constructor, and one to allow setting of both data members

--- parts/ivec2.js
+++ parts/ivec2.js
@@ -14,5 +14,17 @@ IVec2.prototype.Type = function() {
 IVec2.prototype.Output = function() {
 	return "(" + this.mX + ", " + this.mY + ")";
 };
+
+// 
+IVec2.prototype.Copy = function(other) {
+	this.mX = other.mX;
+	this.mY = other.mY;
+};
+
+// 
+IVec2.prototype.Set = function(x, y) {
+	this.mX = x; // x value of our 2d vector
+	this.mY = y; // y value of our 2d vector
+};
 // ...End
 
