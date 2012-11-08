f085d01f38d926f18bc897f8ce2c34a77c61985b
dakodun <jinto@chikin.net>
2012-11-08 13:28:04 +0000

    updated to new build

--- main.js
+++ main.js
@@ -58,175 +58,228 @@ Exception.prototype.What = function() {
 
 
 // SceneManager Class...
+// handles the creation and destruction of scenes, changing between scenes and storing and restoring persistent scenes
 function SceneManager() {
-	this.mCurrScene = null;
-	this.mSceneStore = new Array();
+	this.mCurrScene = null; // our current scene
+	this.mSceneStore = new Array(); // all of our stored (persistent) scenes
 }
 
+// initialises the scene manager
 SceneManager.prototype.SetUp = function() {
 	
 }
 
+// cleans up the scene manager and all scenes currently stored
 SceneManager.prototype.TearDown = function() {
+	// for all currently stored scenes
 	for (var i = 0; i < this.mSceneStore.length; ++i) {
-		this.mSceneStore[i].TearDown();
+		this.mSceneStore[i].TearDown(); // clean up
 	}
 	
-	this.mSceneStore.splice(0, this.mSceneStore.length);
-	this.mCurrScene = NULL;
+	this.mSceneStore.splice(0, this.mSceneStore.length); // remove all scenes
+	this.mCurrScene = null; // set current scene to null
 }
 
+// switches between scenes, handling any persistence
 SceneManager.prototype.ChangeScene = function(newScene) {
-	var found = false;
+	var found = false; // indicates if we have found a previously stored scene
 	
+	// if we have a current scene (i.e., this is not our initial scene change on game start up)
 	if (this.mCurrScene != null) {
+		// if this scene is to be persistent
 		if (this.mCurrScene.Persistent() == true) {
-			this.mSceneStore.push(this.mCurrScene);
+			this.mSceneStore.push(this.mCurrScene); // store this scene
 		}
 		else {
-			this.mCurrScene.TearDown();
+			this.mCurrScene.TearDown(); // otherwise clean up and destroy this scene
 		}
 	}
 	
+	// for all currently stored (persistent) scenes
 	for (var i = 0; i < this.mSceneStore.length; ++i) {
+		// if we find a match
 		if (this.mSceneStore[i].Type() == newScene.Type()) {
-			this.mCurrScene = this.mSceneStore[i];
-			this.mSceneStore.splice(i, i + 1);
-			found = true;
+			this.mCurrScene = this.mSceneStore[i]; // restore the stored scene as our current scene
+			this.mSceneStore.splice(i, i + 1); // remove it from the store
+			found = true; // indicate we have found a persistent scene to restore
 			break;
 		}
 	}
 	
+	// if we didn't find a scene to restore
 	if (found == false) {
-		this.mCurrScene = newScene;
-		this.mCurrScene.SetUp();
+		this.mCurrScene = newScene; // create a new scene
+		this.mCurrScene.SetUp(); // initialise our new scene
 	}
 }
 
+// returns the current scene
 SceneManager.prototype.GetCurrentScene = function() {
 	return this.mCurrScene;
 }
 // ...End
 
 
+// ResourceSort function
+// sorts *Resource objects based on the resource name
 function ResourceSort(first, second) {
 	return second.mResName < first.mResName;
 };
+// ...End
 
 // Resource Class...
+// holds a resource and an associated name
 function Resource(resource, resourceName) {
-	this.mRes = resource;
-	this.mResName = resourceName;
+	this.mRes = resource; // our resource data
+	this.mResName = resourceName; // the id of our resource (string)
 };
 // ...End
 
 // QueuedResource Class...
+// holds a resource name and the location of the resource
 function QueuedResource(resourceName, resourceLocation) {
-	this.mResName = resourceName;
-	this.mResLocation = resourceLocation;
+	this.mResName = resourceName; // the id of our resource (string)
+	this.mResLocation = resourceLocation; // the location of our resource on disk
 };
 // ...End
 
 
 // ResourceStore Class...
+// holds a specific type of resource and handles loading, retrieving and destruction
 function ResourceStore() {
-	this.mStore = new Array();
+	this.mStore = new Array(); // our stored resources
 };
 
+// creates a resource and adds it to our store, returning a handle to it
 ResourceStore.prototype.AddResource = function(resource, resourceName) {
-	// replace with a binary search
+	// replace with a binary search; queue already sorted, use more efficient insert
 	
+	// for all our stored resources
 	for (var i = 0; i < this.mStore.length; ++i) {
+		// if we find a match to the one we are trying to add then error
 		if (this.mStore[i].mResName == resourceName) {
 			throw Exception("Resource already exists.");
 		}
 	}
 	
-	this.mStore.push(new Resource(resource, resourceName));
-	this.mStore.sort(ResourceSort);
+	this.mStore.push(new Resource(resource, resourceName)); // add to the store
+	this.mStore.sort(ResourceSort); // sort the store
 	
-	return this.GetResource(resourceName);
+	return this.GetResource(resourceName); // return our new resource
 };
 
+// removes a resource from the store, cleaning up after it
 ResourceStore.prototype.RemoveResource = function(resourceName) {
 	// replace with a binary search
 	
+	// for all our stored resources
 	for (var i = 0; i < this.mStore.length; ++i) {
+		// if we find a match to the one we are trying to remove
 		if (this.mStore[i].mResName == resourceName) {
-			this.mStore.splice(i, i + 1);
+			this.mStore[i].TearDown(); // perform cleanup
+			this.mStore.splice(i, i + 1); // remove it from the store
 		}
 	}
 	
+	// otherwise error
 	throw Exception("Resource doesn't exist.");
 };
 
+// returns a handle to a stored resource if found
 ResourceStore.prototype.GetResource = function(resourceName) {
 	// replace with a binary search
 	
+	// for all our stored resources
 	for (var i = 0; i < this.mStore.length; ++i) {
+		// if we find a match to the one we are trying to retrieve
 		if (this.mStore[i].mResName == resourceName) {
-			return this.mStore[i].mRes;
+			return this.mStore[i].mRes; // return it
 		}
 	}
 	
+	// otherwise error
 	throw Exception("Resource not found.");
 };
 // ...End
 
 
 // ResourceStore Class...
+// handles the loading of a batch of asynchronous resources such as images or sounds
 function ResourceLoader() {
-	this.mTexQueue = new Array();
+	this.mTexQueue = new Array(); // the queue of unprocessed resources
 	
-	this.mWorking = false;
-	this.mIntervalID = null;
+	this.mWorking = false; // indicates if our resourceloader is currently working
+	this.mIntervalID = null; // the handle of the interval that is checking the state of the resources
 };
 
+// adds a texture to the queue for future processing
 ResourceLoader.prototype.QueueTexture = function(texName, texLocation) {
-	// replace with a binary search
+	// replace with a binary search; queue already sorted, use more efficient insert
 	
+	// if we are currently processing resources then error
 	if (this.mWorking == true) {
 		throw Exception("Resource loader already working.");
 	}
 	
+	// for all textures in the queue
 	for (var i = 0; i < this.mTexQueue.length; ++i) {
+		// if we find a match to the one we are trying to add then error
 		if (this.mTexQueue[i].mResName == texName) {
 			throw Exception("Resource already exists.");
 		}
 	}
 	
-	this.mTexQueue.push(new QueuedResource(texName, texLocation));
-	this.mTexQueue.sort(ResourceSort);
+	this.mTexQueue.push(new QueuedResource(texName, texLocation)); // add to the queue
+	this.mTexQueue.sort(ResourceSort); // sort the queue
 }
 
+// processes all resources currently in the queue
 ResourceLoader.prototype.AcquireResources = function() {
-	this.mWorking = true;
+	this.mWorking = true; // indicate we are currently working
 	
+	// for all textures in the queue
 	for (var i = 0; i < this.mTexQueue.length; ++i) {
+		// add texture to resource manager and load the associated image
 		var tex = nmgrs.resMan.mTexStore.AddResource(new Texture(), this.mTexQueue[i].mResName);
 		tex.LoadFromFile(this.mTexQueue[i].mResLocation);
 	}
 }
 
+// periodically checks the progress of our resource loader
 ResourceLoader.prototype.ProgressCheck = function() {
-	for (var i = 0; i < this.mTexQueue.length; ++i) {
-		var tex = nmgrs.resMan.mTexStore.GetResource(this.mTexQueue[i].mResName);
-		if (tex.mImg.mLoaded == "load" || tex.mImg.mLoaded == "abort" || tex.mImg.mLoaded == "error") {
-			this.mTexQueue.splice(i, 1);
+	// if we are currently working (otherwise no progress will be made)
+	if (this.mWorking == true) {
+		// for all textures in the queue
+		for (var i = 0; i < this.mTexQueue.length; ++i) {
+			// check if the texture has finished loading, whether or not it was successful
+			var tex = nmgrs.resMan.mTexStore.GetResource(this.mTexQueue[i].mResName);
+			if (tex.mImg.mLoaded == "load" || tex.mImg.mLoaded == "abort" || tex.mImg.mLoaded == "error") {
+				this.mTexQueue.splice(i, 1); // remove the texture from the unprocessed queue
+			}
+		}
+		
+		// if our unprocessed queue is now empty
+		if (this.mTexQueue.length == 0) {
+			this.mWorking = false; // we are finished working
+			clearInterval(this.mIntervalID); // stop checking for progress
+			this.mIntervalID = null; // clear interval handle
 		}
 	}
-	
-	if (this.mTexQueue.length == 0) {
-		this.mWorking = false;
-		clearInterval(this.mIntervalID);
+	else {
+		// if called by an interval
+		if (this.mIntervalID != null) {
+			clearInterval(this.mIntervalID); // function called in error, stop future calls
+			this.mIntervalID = null; // clear interval handle
+		}
 	}
 }
 // ...End
 
 
 // ResourceManager Class...
+// holds the resource stores for each individual resource type
 function ResourceManager() {
-	this.mTexStore = new ResourceStore();
+	this.mTexStore = new ResourceStore(); // storage for our textures
 };
 // ...End
 
@@ -234,17 +287,20 @@ function ResourceManager() {
 // Texture Class...
 // a texture (wrapper for javascript Image)
 function Texture() {
-	this.mImg = new Image();
-	this.mImg.mLoaded = "";
+	this.mImg = new Image(); // the image associated with our texture
+	this.mImg.mLoaded = ""; // the load status of our image
 	
+	// called when the image successfully loads
 	this.mImg.onload = function() {
 		this.mLoaded = "load";
 	}
 	
+	// called when the image loading is cancelled
 	this.mImg.onabort = function() {
 		this.mLoaded = "abort";
 	}
 	
+	// called when the image fails to load
 	this.mImg.onerror = function() {
 		this.mLoaded = "error";
 	}
@@ -257,8 +313,8 @@ Texture.prototype.Type = function() {
 
 // loads a texture from a file
 Texture.prototype.LoadFromFile = function(source) {
-	this.mImg.mLoaded = "";
-	this.mImg.src = source;
+	this.mImg.mLoaded = ""; // reset our loading status to blank
+	this.mImg.src = source; // attempt to load our image
 }
 // ...End
 
@@ -269,16 +325,16 @@ Texture.prototype.LoadFromFile = function(source) {
 function Timer() {
 	this.startTime = 0; // the time that this timer was started
 	
-	this.Reset();
+	this.Reset(); // initially reset our timer
 };
 
-// 
+// resets the timer (sets it to the current time)
 Timer.prototype.Reset = function() {
 	var d = new Date();
 	this.startTime = d.getTime(); // set the start time to the current time
 };
 
-// 
+// get the time that has passed since our last reset
 Timer.prototype.GetElapsedTime = function() {
 	var d = new Date();
 	return d.getTime() - this.startTime; // return how much time has elapsed since last call to reset
@@ -295,7 +351,6 @@ this.MAPBOUNDMAX = new IVec2(300, 1200); // maximum size a map can be
 
 
 // MapGenerator Class...
-// mapDimensions is an IVec2
 function MapGenerator(mapDimensions) {
 	if (mapDimensions.Type() == "IVec2") {
         this.mMapDimensions = mapDimensions;
@@ -331,6 +386,7 @@ MapGenerator.prototype.Render = function() {
 
 
 // InitScene Class...
+// self contained parts of the game such as different screens, levels or game modes
 function InitScene() {
 	this.persist = false;
 	// this.resLoad = new ResourceLoader();
@@ -341,11 +397,12 @@ InitScene.prototype.Type = function() {
 	return "InitScene";
 };
 
-// 
+// returns whether this scene is to persist or not (when changing to a new scene -- preserves state)
 InitScene.prototype.Persistent = function() {
 	return persist;
 };
 
+// initialises the scene object
 InitScene.prototype.SetUp = function() {
 	// var tex = new Texture();
 	// tex.LoadFromFile("./res/vis/test.png");
@@ -364,18 +421,22 @@ InitScene.prototype.SetUp = function() {
 	}
 }
 
+// cleans up the scene object
 InitScene.prototype.TearDown = function() {
 	
 }
 
+// handles user input
 InitScene.prototype.Input = function() {
 	
 }
 
+// handles game logic
 InitScene.prototype.Process = function() {
 	
 }
 
+// handles all drawing tasks
 InitScene.prototype.Render = function() {
 	// var tex = new Texture();
 	// tex.LoadFromFile("./res/vis/test.png");
@@ -397,13 +458,13 @@ InitScene.prototype.Render = function() {
 // Game Class...
 // a game object contains all the logic and data of our game
 function Game() {
-	this.mGameLoop = null;
-	this.mFrameLimit = 60;
-	this.mAccum = 0.0;
-	this.mTimer = new Timer();
+	this.mGameLoop = null; // handle to the setInterval that runs our loop code
+	this.mFrameLimit = 60; // the maximum frames per second
+	this.mAccum = 0.0; // the current frame time accumulator
+	this.mTimer = new Timer(); // the timer that handles our main loop timing
 	
-	this.mCanvas = null;
-	this.mContext = null;
+	this.mCanvas = null; // handle to the canvas object
+	this.mContext = null; // handle to the 2d context returned by the canvas object
 };
 
 // initialises the game object
@@ -411,7 +472,7 @@ Game.prototype.SetUp = function() {
 	this.mCanvas = document.getElementById("canvas"); // get the canvas element handle by id from the html file
 	this.mContext = this.mCanvas.getContext("2d"); // get a 2d context handle from the canvas
 	
-	nmgrs.sceneMan.ChangeScene(new InitScene());
+	nmgrs.sceneMan.ChangeScene(new InitScene()); // change to our initial scene
 };
 
 // cleans up the game object
@@ -419,41 +480,49 @@ Game.prototype.TearDown = function() {
 	
 };
 
+// our main game loop
 Game.prototype.Run = function() {
-	var updateDisplay = false;
+	var updateDisplay = false; // do we need to redisplay?
 	
 	this.Input(); // perform input handling
 	
-	var dt = (this.mTimer.GetElapsedTime() / 1000);
-	this.mTimer.Reset();
-	this.mAccum += dt;
+	var dt = (this.mTimer.GetElapsedTime() / 1000); // get the delta time (since last frame)
+	this.mTimer.Reset(); // reset the timer to time next frame
+	this.mAccum += dt; // add the delta time to our accumulated time
+	
+	// while our accumulated time is greater than the frame limit
 	while (this.mAccum > (1 / this.mFrameLimit)) {
 		this.Process(); // process the game
-		this.mAccum -= (1 / this.mFrameLimit);
+		this.mAccum -= (1 / this.mFrameLimit); // decrease the accumulator
 		
 		// interpolate for smoother running, baby
 		
-		updateDisplay = true;
+		updateDisplay = true; // we need to redisplay
 	}
 	
+	// if we need to redisplay
 	if (updateDisplay == true) {
 		this.Render(); // render the results
 	}
 }
 
+// quits tha game (not strictly required, could be used to completely restart the game)
 Game.prototype.Quit = function() {
-	clearInterval(this.mGameLoop);
-	this.TearDown();
+	clearInterval(this.mGameLoop); // remove the interval running our game loop
+	this.TearDown(); // clean up the game object
 }
 
+// handles user input
 Game.prototype.Input = function() {
 	nmgrs.sceneMan.GetCurrentScene().Input(); // perform input for the current scene
 }
 
+// handles game logic
 Game.prototype.Process = function() {
 	nmgrs.sceneMan.GetCurrentScene().Process(); // process the current scene
 }
 
+// handles all drawing tasks
 Game.prototype.Render = function() {
 	nmgrs.sceneMan.GetCurrentScene().Render(); // render the current scene
 }
