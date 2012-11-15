7757912141be1f299dde2abd56bc3f82b8b2320d
dakodun <jinto@chikin.net>
2012-11-15 21:14:37 +0000

    added comments; add rotation and height parameters

--- parts/text.js
+++ parts/text.js
@@ -1,5 +1,5 @@
 // Text Class...
-// 
+// renderable text
 function Text() {
 	this.mFont = "12px Arial";
 	this.mString = "";
@@ -8,15 +8,16 @@ function Text() {
 	
 	this.mPos = new IVec2(0, 12);
 	this.mOutline = false;
+	this.mRotation = 0;
+	this.mHeight = 12;
 }
-
 
 // returns the type of this object for validity checking
 Text.prototype.Type = function() {
 	return "Text";
 }
 
-//
+// make a copy of another (other) text (copy constructor)
 Text.prototype.Copy = function(other) {
 	this.mFont = other.mFont;
 	this.mString = other.mString;
@@ -25,11 +26,19 @@ Text.prototype.Copy = function(other) {
 	
 	this.mPos.Copy(other.mPos);
 	this.mOutline = other.mOutline;
+	this.mRotation = other.mRotation;
+	this.mHeight = other.mHeight;
 }
 
+// return the width of the text
 Text.prototype.GetWidth = function() {
 	nmain.game.mCurrContext.font = this.mFont;
 	return nmain.game.mCurrContext.measureText(this.mString).width;
+}
+
+// return the height of the text
+Text.prototype.GetHeight = function() {
+	return this.mHeight;
 }
 // ...End
 
