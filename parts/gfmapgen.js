02d10d6e881bd268c163427902b8994cbc0603f4
dakodun <jinto@chikin.net>
2012-11-16 18:09:33 +0000

    more work on map generation - base area is created and the map is 'eroded' into a more interesting shape

--- parts/gfmapgen.js
+++ parts/gfmapgen.js
@@ -4,7 +4,6 @@ function GFMapGen() {
 };
 
 GFMapGen.prototype.GenerateMap = function(seed, size, baseSize) {
-	var rand = new RNG(seed);
 	var map = new GFMap();
 	
 	var dimX = 0; var dimY = 0;
@@ -22,31 +21,51 @@ GFMapGen.prototype.GenerateMap = function(seed, size, baseSize) {
 			dimY = 18;
 		}
 		
+		map.mRand.SetSeed(seed);
 		map.SetUp(new IVec2(dimX, dimY));
 	}
 	
 	var rowNum = 0;
+	var baseRadius = 0;
 	{
 		if (baseSize == "s") {
-			rowNum = rand.GetRandInt(1, 2);
+			rowNum = map.mRand.GetRandInt(1, 2);
+			baseRadius = 1;
 		}
 		else if (baseSize == "m") {
-			rowNum = rand.GetRandInt(2, 4);
+			rowNum = map.mRand.GetRandInt(1, 3);
+			baseRadius = map.mRand.GetRandInt(1, 4);
 		}
 		else {
-			rowNum = rand.GetRandInt(4, 6);
+			rowNum = map.mRand.GetRandInt(2, 4);
+			baseRadius = map.mRand.GetRandInt(3, 6);
 		}
 	}
 	
 	var idLow = 0;
 	var idHigh = (dimX * rowNum) - 1;
-	var base1 = rand.GetRandInt(idLow, idHigh);
-	map.mMapTiles[base1].mSprite.SetCurrentFrame(2);
+	var base1 = map.mRand.GetRandInt(idLow, idHigh);
+	// map.mMapTiles[base1].mSprite.SetCurrentFrame(map.mRand.GetRandInt(10, 14));
 	
 	idLow = dimX * (dimY - rowNum);
 	idHigh = (dimX * dimY) - 1;
-	var base2 = rand.GetRandInt(idLow, idHigh);
-	map.mMapTiles[base2].mSprite.SetCurrentFrame(3);
+	var base2 = map.mRand.GetRandInt(idLow, idHigh);
+	// map.mMapTiles[base2].mSprite.SetCurrentFrame(map.mRand.GetRandInt(15, 19));
+	
+	var baseCircles = new Array();
+	baseCircles.push(new GFMapCircle(map.mMapTiles[base1].mPos, 10 * baseRadius));
+	baseCircles.push(new GFMapCircle(map.mMapTiles[base2].mPos, 10 * baseRadius));
+	
+	map.CreateBases(baseCircles);
+	
+	var mapCircles = new Array();
+	var curRow = map.mRand.GetRandInt(1, 3);
+	while (curRow <= dimY) {
+		mapCircles.push(new GFMapCircle(new IVec2(map.mRand.GetRandInt(0, dimX - 1), curRow), 10 * map.mRand.GetRandInt(1, 4)));
+		curRow += map.mRand.GetRandInt(1, 3);
+	}
+	
+	map.Erode(mapCircles);
 	
 	return map;
 	// 46 - hypoteneuse
