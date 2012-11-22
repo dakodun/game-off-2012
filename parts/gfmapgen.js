b2a05d88bae354acf47761c1f2f5341b8acd6393
dakodun <jinto@chikin.net>
2012-11-22 07:18:58 +0000

    changed map generation from intersecting circles to 'ants'

--- parts/gfmapgen.js
+++ parts/gfmapgen.js
@@ -7,81 +7,95 @@ GFMapGen.prototype.GenerateMap = function(seed, size, baseSize) {
 	var map = new GFMap();
 	
 	var dimX = 0; var dimY = 0;
+	var lzLeft = 0; var lzRight = 0;
+	var numAnts = 0;
 	{
 		if (size == "s") {
-			dimX = 6;
-			dimY = 10;
+			dimX = 9;
+			dimY = 29;
+			
+			lzLeft = -2;
+			lzRight = 2;
+			
+			numAnts = map.mRand.GetRandInt(2, 4);
 		}
 		else if (size == "m") {
-			dimX = 8;
-			dimY = 14;
+			dimX = 14;
+			dimY = 37;
+			
+			lzLeft = -3;
+			lzRight = 3;
+			
+			numAnts = map.mRand.GetRandInt(3, 5);
 		}
 		else {
-			dimX = 10;
-			dimY = 18;
+			dimX = 17;
+			dimY = 45;
+			
+			lzLeft = -4;
+			lzRight = 4;
+			
+			numAnts = map.mRand.GetRandInt(5, 8);
 		}
 		
 		map.mRand.SetSeed(seed);
 		map.SetUp(new IVec2(dimX, dimY));
 	}
 	
-	var rowNum = 0;
-	var baseRadius = 0;
+	// Fill top n rows as base
+	// Fill bottom n rows as ground
+	// Create landing zone within lower ground
+	
+	var enemyBaseSize = 0;
 	{
 		if (baseSize == "s") {
-			rowNum = map.mRand.GetRandInt(1, 2);
-			baseRadius = 1;
+			enemyBaseSize = 2;
 		}
 		else if (baseSize == "m") {
-			rowNum = map.mRand.GetRandInt(1, 3);
-			baseRadius = map.mRand.GetRandInt(1, 4);
+			enemyBaseSize = map.mRand.GetRandInt(2, 3);
 		}
 		else {
-			rowNum = map.mRand.GetRandInt(2, 4);
-			baseRadius = map.mRand.GetRandInt(3, 6);
+			enemyBaseSize = map.mRand.GetRandInt(3, 4);
 		}
 	}
 	
-	var idLow = 0;
-	var idHigh = (dimX * rowNum) - 1;
-	var base1 = map.mRand.GetRandInt(idLow, idHigh);
-	// map.mMapTiles[base1].mSprite.SetCurrentFrame(map.mRand.GetRandInt(10, 14));
-	
-	idLow = dimX * (dimY - rowNum);
-	idHigh = (dimX * dimY) - 1;
-	var base2 = map.mRand.GetRandInt(idLow, idHigh);
-	// map.mMapTiles[base2].mSprite.SetCurrentFrame(map.mRand.GetRandInt(15, 19));
-	
-	var baseCircles = new Array();
-	baseCircles.push(new GFMapCircle(map.mMapTiles[base1].mPos, 10 * baseRadius));
-	baseCircles.push(new GFMapCircle(map.mMapTiles[base2].mPos, 10 * baseRadius));
-	
-	map.CreateBases(baseCircles);
-	
-	var mapCircles = new Array();
-	var curRow = map.mRand.GetRandInt(1, 3);
-	while (curRow <= dimY) {
-		mapCircles.push(new GFMapCircle(new IVec2(map.mRand.GetRandInt(0, dimX - 1), curRow), 10 * map.mRand.GetRandInt(1, 4)));
-		curRow += map.mRand.GetRandInt(1, 3);
+	for (var i = 0; i < (dimX * enemyBaseSize); ++i) {
+		map.mMapTiles[i].mSprite.SetCurrentFrame(map.mRand.GetRandInt(10, 14));
+		map.mMapTiles[i].mType = "red";
 	}
 	
-	map.Erode(mapCircles);
+	for (var i = (dimX * (dimY - 5)); i < (dimX * dimY); ++i) {
+		map.mMapTiles[i].mSprite.SetCurrentFrame(map.mRand.GetRandInt(5, 9));
+	}
+	
+	{
+		var midPoint = Math.floor(dimX / 2);
+		
+		for (var i = 2; i <= 4; ++i) {
+			for (var j = lzLeft; j <= lzRight; ++j) {
+				var tileID = (dimX * (dimY - i)) + midPoint + j;
+				map.mMapTiles[tileID].mSprite.SetCurrentFrame(map.mRand.GetRandInt(15, 19));
+				map.mMapTiles[tileID].mType = "blue";
+			}
+		}
+	}
+	
+	{
+		for (var i = 0; i < numAnts; ++i) {
+			var x = map.mRand.GetRandInt(0, dimX - 1);
+			
+			var ant = new GFMapAnt(new IVec2(x, dimY - 6));
+			var arr = ant.Dig(map, dimX);
+			for (var j = 0; j < arr.length; ++j) {
+				var id = arr[j].mX + (arr[j].mY * dimX);
+				if (map.mMapTiles[id].mType == "") {
+					map.mMapTiles[id].mSprite.SetCurrentFrame(map.mRand.GetRandInt(5, 9));
+				}
+			}
+		}
+	}
 	
 	return map;
-	// 46 - hypoteneuse
-	
-	
-	/* if () {
-		
-	} */
 }
-
-/*
-create map with bounds (small, medium and large)
-define base size(s)
-  create bases at top and bottom of map via circle intersection method
-  change base frame to corresponding colour
-*/
-
 // ...End
 
