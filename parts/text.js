d6050fa51dc342a66c37334a63a46edf28d668ab
dakodun <jinto@chikin.net>
2012-11-22 07:14:35 +0000

    seperated font into size and style components; added support for multi-line strings, delimited by '\n'

--- parts/text.js
+++ parts/text.js
@@ -2,6 +2,9 @@
 // renderable text
 function Text() {
 	this.mFont = "12px Arial";
+	this.mFontSize = "12";
+	this.mFontName = "Arial";
+	
 	this.mString = "";
 	this.mColour = "#FFFFFF";
 	this.mDepth = 0;
@@ -20,6 +23,9 @@ Text.prototype.Type = function() {
 // make a copy of another (other) text (copy constructor)
 Text.prototype.Copy = function(other) {
 	this.mFont = other.mFont;
+	this.mFontSize = other.mFontSize;
+	this.mFontName = other.mFontName;
+	
 	this.mString = other.mString;
 	this.mColour = other.mColour;
 	this.mDepth = other.mDepth;
@@ -33,12 +39,35 @@ Text.prototype.Copy = function(other) {
 // return the width of the text
 Text.prototype.GetWidth = function() {
 	nmain.game.mCurrContext.font = this.mFont;
-	return nmain.game.mCurrContext.measureText(this.mString).width;
+	
+	var txtArr = this.mString.split("\n");
+	var longest = 0;
+	for (var i = 0; i < txtArr.length; ++i) {
+		var strLen = nmain.game.mCurrContext.measureText(txtArr[i]).width;
+		if (strLen > longest) {
+			longest = strLen;
+		}
+	}
+	
+	return strLen;
 }
 
 // return the height of the text
 Text.prototype.GetHeight = function() {
 	return this.mHeight;
+}
+
+// 
+Text.prototype.SetFontSize = function(size) {
+	this.mFontSize = size.toString();
+	this.mFont = this.mFontSize + "px " + this.mFontName;
+	this.mHeight = size;
+}
+
+// 
+Text.prototype.SetFontName = function(name) {
+	this.mFontName = name;
+	this.mFont = this.mFontSize + " " + this.mFontName;
 }
 // ...End
 
