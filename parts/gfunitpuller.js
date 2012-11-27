3c6b8f4be4197c683d77fc7ba48d259e28459cbf
dakodun <jinto@chikin.net>
2012-11-27 21:16:21 +0000

    added moves left memeber and a sprite and accompanying logic to indicate that; added unit movement logic

--- parts/gfunitpuller.js
+++ parts/gfunitpuller.js
@@ -12,15 +12,30 @@ function GFUnitPuller() {
 	
 	this.mUI = new GFUnitUI();
 	this.mPlacementInfo = "";
+	
+	this.mMovesLeft = 2;
+	this.mMovesLeftSprite = new Sprite();
 }
 
-GFUnitPuller.prototype.SetUp = function(camera, tex, pos) {
+GFUnitPuller.prototype.SetUp = function(camera, pos) {
 	this.mPos.Copy(pos);
 	
-	this.mSprite.SetAnimatedTexture(tex, 4, 4, 14 / nmain.game.mFrameLimit, -1);
-	this.mSprite.mOrigin.Set(0, 0);
-	this.mSprite.mPos.Set(pos.mX * 32, pos.mY * 32);
-	this.mSprite.mDepth = -500 - nmgrs.sceneMan.mCurrScene.mMap.PosToID(pos);
+	{
+		var tex = nmgrs.resMan.mTexStore.GetResource("unit_u_puller");
+		this.mSprite.SetAnimatedTexture(tex, 4, 4, 14 / nmain.game.mFrameLimit, -1);
+		this.mSprite.mOrigin.Set(0, 0);
+		this.mSprite.mPos.Set(pos.mX * 32, pos.mY * 32);
+		this.mSprite.mDepth = -500 - nmgrs.sceneMan.mCurrScene.mMap.PosToID(pos);
+	}
+	
+	{
+		var tex = nmgrs.resMan.mTexStore.GetResource("gui_moves");
+		this.mMovesLeftSprite.SetAnimatedTexture(tex, 3, 1, -1, -1);
+		this.mMovesLeftSprite.mOrigin.Set(20, 34);
+		this.mMovesLeftSprite.mPos.Set(pos.mX * 32, pos.mY * 32);
+		this.mMovesLeftSprite.SetCurrentFrame(0);
+		this.mMovesLeftSprite.mDepth = -2000;
+	}
 	
 	this.mBound.mOutline = true;
 	this.mBound.mColour = "#FF1111";
@@ -107,7 +122,38 @@ GFUnitPuller.prototype.ProcessUI = function(camera) {
 				
 				if (util.PointInRectangle(pt, tl, br) == true) {
 					if (i == 0) {
-						// button 1
+						var tex = nmgrs.resMan.mTexStore.GetResource("tile_hilite");
+						var boundsArr = new Array();
+						var hiliteArr = new Array();
+						
+						var arr = new Array();
+						arr = arr.concat(this.CheckValidMove());
+						
+						for (var j = 0; j < arr.length; ++j) {
+							var id = arr[j];
+							var pos = nmgrs.sceneMan.mCurrScene.mMap.IDToPos(id);
+							
+							if (nmgrs.sceneMan.mCurrScene.mMap.mMapTiles[id].mFree == true) {
+								var tl = new IVec2();
+								tl.Set(pos.mX * 32, pos.mY * 32);
+								boundsArr.push(tl);
+								
+								var br = new IVec2();
+								br.Set((pos.mX * 32) + 32, (pos.mY * 32) + 32);
+								boundsArr.push(br);
+								
+								var spr = new Sprite();
+								spr.SetAnimatedTexture(tex, 8, 4, 3 / nmain.game.mFrameLimit, -1);
+								spr.mOrigin.Set(8, 8);
+								spr.mPos.Set(pos.mX * 32, pos.mY * 32);
+								spr.mDepth = 999 + (nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mX * nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mY) - id;
+								
+								hiliteArr.push(spr);
+							}
+						}
+						
+						this.mPlacementInfo = "move";
+						nmgrs.sceneMan.mCurrScene.TogglePlacementMode(true, boundsArr, hiliteArr);
 					}
 					else if (i == 1) {
 						// button 2
@@ -129,17 +175,19 @@ GFUnitPuller.prototype.ProcessUI = function(camera) {
 }
 
 GFUnitPuller.prototype.SetActive = function(active) {
-	if (this.mActive != active) {
+	// if (this.mActive != active) {
 		var tex = nmgrs.resMan.mTexStore.GetResource("unit_u_puller");
 		this.mActive = active;
 		
 		if (this.mActive == true) {
+			this.mMovesLeft = 2;
+			this.mMovesLeftSprite.SetCurrentFrame(0);
 			this.mSprite.SetAnimatedTextureSegment(tex, 8, 4, 14 / nmain.game.mFrameLimit, 0, 3, -1);
 		}
 		else {
 			this.mSprite.SetAnimatedTextureSegment(tex, 8, 4, -1, 4, 4, -1);
 		}
-	}
+	// }
 }
 
 GFUnitPuller.prototype.GetRender = function() {
@@ -150,6 +198,7 @@ GFUnitPuller.prototype.GetRender = function() {
 	}
 	
 	if (this.mSelected == true) {
+		arr.push(this.mMovesLeftSprite);
 		arr = arr.concat(this.mUI.GetRender());
 	}
 	
@@ -157,7 +206,213 @@ GFUnitPuller.prototype.GetRender = function() {
 }
 
 GFUnitPuller.prototype.PlacementCallback = function(info, id) {
+	if (info == "move") {
+		nmgrs.sceneMan.mCurrScene.mMap.mMapTiles[nmgrs.sceneMan.mCurrScene.mMap.PosToID(this.mPos)].mFree = true;
+		nmgrs.sceneMan.mCurrScene.mMap.mMapTiles[id].mFree = false;
+		this.mPos.Copy(nmgrs.sceneMan.mCurrScene.mMap.IDToPos(id));
+		
+		this.mSprite.mPos.Set(this.mPos.mX * 32, this.mPos.mY * 32);
+		this.mSprite.mDepth = -500 - id;
+		this.mMovesLeftSprite.mPos.Set(this.mPos.mX * 32, this.mPos.mY * 32);
+		this.mBound.mPos.Set(this.mPos.mX * 32, this.mPos.mY * 32);
+		
+		this.mMovesLeft--;
+		this.mMovesLeftSprite.SetCurrentFrame(2 - this.mMovesLeft);
+		
+		if (this.mMovesLeft == 0) {
+			if (nmgrs.sceneMan.mCurrScene.mSelectID >= 0) {
+				this.SetActive(false);
+				this.mSelected = false;
+				nmgrs.sceneMan.mCurrScene.mSelectID = -1;
+			}
+		}
+		
+		this.mPlacementInfo = "";
+		nmgrs.sceneMan.mCurrScene.TogglePlacementMode(false, null, null);
+	}
+}
+
+GFUnitPuller.prototype.CheckValidMove = function() {
+	var moveAmount = 3;
+	var closedTiles = new Array();
+	var openTiles = new Array();
+	openTiles.push(nmgrs.sceneMan.mCurrScene.mMap.PosToID(this.mPos));
 	
+	for (var j = 0; j < moveAmount + 1; ++j) {
+		var pendingOpenTiles = new Array();
+		
+		while (openTiles.length > 0) {			
+			// check tile to the left
+			// if current tile isn't at 0 (left boundary)
+			if ((openTiles[0] % nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mX) > 0) {
+				var idToCheck = openTiles[0] - 1;
+				
+				// if tile to the left is free (valid move)
+				if (nmgrs.sceneMan.mCurrScene.mMap.mMapTiles[idToCheck].mFree == true) {
+					// if tile isn't on either the closed or the open list
+					var found = false;
+					for (var k = 0; k < openTiles.length; ++k) {
+						if (openTiles[k] == idToCheck) {
+							found = true;
+							break;
+						}
+					}
+					
+					if (found == false) {
+						for (var k = 0; k < pendingOpenTiles.length; ++k) {
+							if (pendingOpenTiles[k] == idToCheck) {
+								found = true;
+								break;
+							}
+						}
+					}
+					
+					if (found == false) {
+						for (var k = 0; k < closedTiles.length; ++k) {
+							if (closedTiles[k] == idToCheck) {
+								found = true;
+								break;
+							}
+						}
+					}
+					
+					if (found == false) {
+						pendingOpenTiles.push(idToCheck);
+					}
+				}
+			}
+			
+			// check tile above
+			// if current tile isn't at 0 (top boundary)
+			if (Math.floor(openTiles[0] / nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mX) > 0) {
+				var idToCheck = openTiles[0] - nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mX;
+				
+				// if tile above is free (valid move)
+				if (nmgrs.sceneMan.mCurrScene.mMap.mMapTiles[idToCheck].mFree == true) {
+					// if tile isn't on either the closed or the open list
+					var found = false;
+					for (var k = 0; k < openTiles.length; ++k) {
+						if (openTiles[k] == idToCheck) {
+							found = true;
+							break;
+						}
+					}
+					
+					if (found == false) {
+						for (var k = 0; k < pendingOpenTiles.length; ++k) {
+							if (pendingOpenTiles[k] == idToCheck) {
+								found = true;
+								break;
+							}
+						}
+					}
+					
+					if (found == false) {
+						for (var k = 0; k < closedTiles.length; ++k) {
+							if (closedTiles[k] == idToCheck) {
+								found = true;
+								break;
+							}
+						}
+					}
+					
+					if (found == false) {
+						pendingOpenTiles.push(idToCheck);
+					}
+				}
+			}
+			
+			// check tile to the right
+			// if current tile isn't at nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mX - 1 (right boundary)
+			if ((openTiles[0] % nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mX) < nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mX - 1) {
+				var idToCheck = openTiles[0] + 1;
+				
+				// if tile to the right is free (valid move)
+				if (nmgrs.sceneMan.mCurrScene.mMap.mMapTiles[idToCheck].mFree == true) {
+					// if tile isn't on either the closed or the open list
+					var found = false;
+					for (var k = 0; k < openTiles.length; ++k) {
+						if (openTiles[k] == idToCheck) {
+							found = true;
+							break;
+						}
+					}
+					
+					if (found == false) {
+						for (var k = 0; k < pendingOpenTiles.length; ++k) {
+							if (pendingOpenTiles[k] == idToCheck) {
+								found = true;
+								break;
+							}
+						}
+					}
+					
+					if (found == false) {
+						for (var k = 0; k < closedTiles.length; ++k) {
+							if (closedTiles[k] == idToCheck) {
+								found = true;
+								break;
+							}
+						}
+					}
+					
+					if (found == false) {
+						pendingOpenTiles.push(idToCheck);
+					}
+				}
+			}
+			
+			// check tile below
+			// if current tile isn't at nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mY - 1 (bottom boundary)
+			if (Math.floor(openTiles[0] / nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mX) < nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mY - 1) {
+				var idToCheck = openTiles[0] + nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mX;
+				
+				// if tile below is free (valid move)
+				if (nmgrs.sceneMan.mCurrScene.mMap.mMapTiles[idToCheck].mFree == true) {
+					// if tile isn't on either the closed or the open list
+					var found = false;
+					for (var k = 0; k < openTiles.length; ++k) {
+						if (openTiles[k] == idToCheck) {
+							found = true;
+							break;
+						}
+					}
+					
+					if (found == false) {
+						for (var k = 0; k < pendingOpenTiles.length; ++k) {
+							if (pendingOpenTiles[k] == idToCheck) {
+								found = true;
+								break;
+							}
+						}
+					}
+					
+					if (found == false) {
+						for (var k = 0; k < closedTiles.length; ++k) {
+							if (closedTiles[k] == idToCheck) {
+								found = true;
+								break;
+							}
+						}
+					}
+					
+					if (found == false) {
+						pendingOpenTiles.push(idToCheck);
+					}
+				}
+			}
+			
+			// add this id to the closed list
+			closedTiles.push(openTiles[0]);
+			
+			// remove from the open list
+			openTiles.splice(0, 1);
+		}
+		
+		openTiles = openTiles.concat(pendingOpenTiles);
+	}
+	
+	return closedTiles;
 }
 // ...End
 
