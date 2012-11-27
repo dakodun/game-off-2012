c136e70c5f3bfb55c61e679c25c0c1776593691c
dakodun <jinto@chikin.net>
2012-11-27 21:11:54 +0000

    loading more required resources

--- parts/initscene.js
+++ parts/initscene.js
@@ -25,6 +25,7 @@ InitScene.prototype.SetUp = function() {
 		nmgrs.resLoad.QueueTexture("turn_2", "./res/vis/turn_2.png");
 		nmgrs.resLoad.QueueTexture("gui_arrow_up", "./res/vis/gui_arrow_up.png");
 		nmgrs.resLoad.QueueTexture("gui_arrow_down", "./res/vis/gui_arrow_down.png");
+		nmgrs.resLoad.QueueTexture("gui_moves", "./res/vis/gui_moves.png");
 		
 		nmgrs.resLoad.QueueTexture("unit_b_workerprod", "./res/vis/unit_b_workerprod.png");
 		nmgrs.resLoad.QueueTexture("gui_workerprod", "./res/vis/gui_workerprod.png");
