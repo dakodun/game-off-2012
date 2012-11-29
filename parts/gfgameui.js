2db37bc6ef8fd6d31b0409923b8535dbe9f05ead
dakodun <jinto@chikin.net>
2012-11-29 20:27:48 +0000

    added support to display debug info; updated controls text

--- parts/gfgameui.js
+++ parts/gfgameui.js
@@ -8,13 +8,12 @@ function GFGameUI() {
 	this.mTurnSprite = new Sprite();
 	
 	this.mControlsText = new Text();
-	// this.mControlsTextHi = new Text();
-	// this.mControlsBack = new Shape();
 	
 	this.mEndTurnTapTextA = new Text();
-	// this.mEndTurnTapTextAHi = new Text();
 	this.mEndTurnTapTextB = new Text();
-	// this.mEndTurnTapTextBHi = new Text();
+	
+	this.mDebugInfo = new Text();
+	this.mShowDebug = false;
 }
 
 GFGameUI.prototype.SetUp = function(camera) {
@@ -41,26 +40,10 @@ GFGameUI.prototype.SetUp = function(camera) {
 	
 	{
 		this.mControlsText.mColour = "#EBEBEB";
-		this.mControlsText.mString = "Up and Down Arrows to scroll.\nDouble-tap E to end your turn.\nLeft Mouse Button to select a unit or UI option.\nMiddle Mouse Button to cancel.";
+		this.mControlsText.mString = "D to toggle debug mode (cheating!).\nUp and Down Arrows to scroll.\nDouble-tap E to end your turn.\nLeft Mouse Button to select a unit or UI option.\nMiddle Mouse Button to cancel.";
 		this.mControlsText.mDepth = -1000;
 		this.mControlsText.mShadow = true;
 		this.mControlsText.mPos.Set(camera.mTranslate.mX + 4, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - this.mControlsText.GetHeight() - 5);
-		
-		/* this.mControlsTextHi.mColour = "#000000";
-		this.mControlsTextHi.mString = "Up and Down Arrow to scroll.\nDouble-tap E to end your turn.";
-		this.mControlsTextHi.mDepth = -999;
-		this.mControlsTextHi.mPos.Set(camera.mTranslate.mX + 5, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - this.mControlsText.mHeight - 6); */
-		
-		/* this.mControlsBack.mColour = "#000000";
-		this.mControlsBack.mDepth = -998;
-		this.mControlsBack.mAlpha = 0.9;
-		this.mControlsBack.mPos.Set(camera.mTranslate.mX + 2, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - (this.mControlsText.mHeight * 2) - 4);
-		
-		var textWidth = this.mControlsText.GetWidth();
-		var textHeight = this.mControlsText.GetHeight() * 2;
-		this.mControlsBack.AddPoint(new IVec2(textWidth + 4, 0));
-		this.mControlsBack.AddPoint(new IVec2(textWidth + 4, textHeight + 2));
-		this.mControlsBack.AddPoint(new IVec2(0, textHeight + 2)); */
 	}
 	
 	{
@@ -71,26 +54,20 @@ GFGameUI.prototype.SetUp = function(camera) {
 		this.mEndTurnTapTextA.mShadow = true;
 		this.mEndTurnTapTextA.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextA.GetWidth() / 2), camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + 12);
 		
-		/* this.mEndTurnTapTextAHi.mColour = "#000000";
-		this.mEndTurnTapTextAHi.SetFontName("sans-serif");
-		this.mEndTurnTapTextAHi.SetFontSize(12);
-		this.mEndTurnTapTextAHi.mString = "Press E Again To";
-		this.mEndTurnTapTextAHi.mDepth = -1999;
-		this.mEndTurnTapTextAHi.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextA.GetWidth() / 2) + 1, camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + 13); */
-		
 		this.mEndTurnTapTextB.SetFontName("sans-serif");
 		this.mEndTurnTapTextB.SetFontSize(32);
 		this.mEndTurnTapTextB.mString = "CONFIRM END TURN";
 		this.mEndTurnTapTextB.mDepth = -2000;
 		this.mEndTurnTapTextB.mShadow = true;
 		this.mEndTurnTapTextB.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextB.GetWidth() / 2), camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + this.mEndTurnTapTextB.GetHeight() + 12);
-		
-		/* this.mEndTurnTapTextBHi.mColour = "#000000";
-		this.mEndTurnTapTextBHi.SetFontName("sans-serif");
-		this.mEndTurnTapTextBHi.SetFontSize(32);
-		this.mEndTurnTapTextBHi.mString = "CONFIRM";
-		this.mEndTurnTapTextBHi.mDepth = -1999;
-		this.mEndTurnTapTextBHi.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextB.GetWidth() / 2) + 1, camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + this.mEndTurnTapTextB.GetHeight() + 13); */
+	}
+	
+	{
+		this.mDebugInfo.mColour = "#EBEBEB";
+		this.mDebugInfo.mString = "FPS (Framelimit): " + Math.round(nmain.game.mFPS) + " (" + nmain.game.mFrameLimit + ")";
+		this.mDebugInfo.mDepth = -10000;
+		this.mDebugInfo.mShadow = true;
+		this.mDebugInfo.mPos.Set(camera.mTranslate.mX + 72, camera.mTranslate.mY + this.mDebugInfo.GetHeight());
 	}
 }
 
@@ -100,18 +77,18 @@ GFGameUI.prototype.Input = function(camera) {
 	this.mArrowDownSprite.mPos.Set(camera.mTranslate.mX + nmain.game.mCanvasSize.mX - 46, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - 64);
 	
 	this.mControlsText.mPos.Set(camera.mTranslate.mX + 4, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - this.mControlsText.GetHeight() - 5);
-	// this.mControlsTextHi.mPos.Set(camera.mTranslate.mX + 5, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - this.mControlsText.mHeight - 6);
-	// this.mControlsBack.mPos.Set(camera.mTranslate.mX + 2, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - (this.mControlsText.mHeight * 2) - 4);
 	
 	this.mEndTurnTapTextA.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextA.GetWidth() / 2), camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + 12);
-	// this.mEndTurnTapTextAHi.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextA.GetWidth() / 2) + 1, camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + 13);
 	this.mEndTurnTapTextB.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextB.GetWidth() / 2), camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + this.mEndTurnTapTextB.GetHeight() + 12);
-	// this.mEndTurnTapTextBHi.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextB.GetWidth() / 2) + 1, camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + this.mEndTurnTapTextB.GetHeight() + 13);
+	
+	this.mDebugInfo.mPos.Set(camera.mTranslate.mX + 72, camera.mTranslate.mY + this.mDebugInfo.GetHeight());
 }
 
 GFGameUI.prototype.Process = function() {
 	this.mArrowUpSprite.Process();
 	this.mArrowDownSprite.Process();
+	
+	this.mDebugInfo.mString = "FPS (Framelimit): " + Math.round(nmain.game.mFPS) + " (" + nmain.game.mFrameLimit + ")";
 }
 
 GFGameUI.prototype.Render = function(camera, turn, mapSize) {
@@ -128,15 +105,15 @@ GFGameUI.prototype.Render = function(camera, turn, mapSize) {
 		}
 	}
 	
-	// this.mDynamicUIBatch.AddShape(this.mControlsBack);
 	this.mDynamicUIBatch.AddText(this.mControlsText);
-	// this.mDynamicUIBatch.AddText(this.mControlsTextHi);
 	
 	if (nmgrs.sceneMan.mCurrScene.mEndPlayerTurn > 0) {
 		this.mDynamicUIBatch.AddText(this.mEndTurnTapTextA);
 		this.mDynamicUIBatch.AddText(this.mEndTurnTapTextB);
-		// this.mDynamicUIBatch.AddText(this.mEndTurnTapTextAHi);
-		// this.mDynamicUIBatch.AddText(this.mEndTurnTapTextBHi);
+	}
+	
+	if (this.mShowDebug == true) {
+		this.mDynamicUIBatch.AddText(this.mDebugInfo);
 	}
 	
 	this.mDynamicUIBatch.Render(camera);
