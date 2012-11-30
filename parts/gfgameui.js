29e4ad5b66b54c9e1ce9122cad19be24b36297ed
dakodun <jinto@chikin.net>
2012-11-30 17:22:31 +0000

    updated gui; added mouse support for ending turn and scrolling

--- parts/gfgameui.js
+++ parts/gfgameui.js
@@ -9,6 +9,7 @@ function GFGameUI() {
 	
 	this.mControlsText = new Text();
 	
+	this.mEndTurnSprite = new Sprite();
 	this.mEndTurnTapTextA = new Text();
 	this.mEndTurnTapTextB = new Text();
 	
@@ -26,15 +27,15 @@ GFGameUI.prototype.SetUp = function(camera) {
 	
 	{
 		var tex = nmgrs.resMan.mTexStore.GetResource("gui_arrow_up");
-		this.mArrowUpSprite.SetAnimatedTexture(tex, 16, 5, 1 / nmain.game.mFrameLimit, -1);
-		this.mArrowUpSprite.mPos.Set(camera.mTranslate.mX + nmain.game.mCanvasSize.mX - 46, camera.mTranslate.mY);
+		this.mArrowUpSprite.SetAnimatedTexture(tex, 12, 4, 4 / nmain.game.mFrameLimit, -1);
+		this.mArrowUpSprite.mPos.Set(camera.mTranslate.mX + nmain.game.mCanvasSize.mX - 64, camera.mTranslate.mY + 4);
 		this.mArrowUpSprite.mDepth = -1000;
 	}
 	
 	{
 		var tex = nmgrs.resMan.mTexStore.GetResource("gui_arrow_down");
-		this.mArrowDownSprite.SetAnimatedTexture(tex, 16, 5, 1 / nmain.game.mFrameLimit, -1);
-		this.mArrowDownSprite.mPos.Set(camera.mTranslate.mX + nmain.game.mCanvasSize.mX - 46, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - 64);
+		this.mArrowDownSprite.SetAnimatedTexture(tex, 12, 4, 4 / nmain.game.mFrameLimit, -1);
+		this.mArrowDownSprite.mPos.Set(camera.mTranslate.mX + nmain.game.mCanvasSize.mX - 64, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - 68);
 		this.mArrowDownSprite.mDepth = -1000;
 	}
 	
@@ -47,9 +48,15 @@ GFGameUI.prototype.SetUp = function(camera) {
 	}
 	
 	{
+		var tex = nmgrs.resMan.mTexStore.GetResource("endturn");
+		this.mEndTurnSprite.SetAnimatedTexture(tex, 2, 1, -1, -1);
+		this.mEndTurnSprite.mPos.Set(camera.mTranslate.mX, camera.mTranslate.mY);
+		this.mEndTurnSprite.mDepth = -1000;
+		this.mEndTurnSprite.SetCurrentFrame(1);
+		
 		this.mEndTurnTapTextA.SetFontName("sans-serif");
 		this.mEndTurnTapTextA.SetFontSize(12);
-		this.mEndTurnTapTextA.mString = "Press E Again To";
+		this.mEndTurnTapTextA.mString = "Press Again To";
 		this.mEndTurnTapTextA.mDepth = -2000;
 		this.mEndTurnTapTextA.mShadow = true;
 		this.mEndTurnTapTextA.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextA.GetWidth() / 2), camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + 12);
@@ -64,31 +71,98 @@ GFGameUI.prototype.SetUp = function(camera) {
 	
 	{
 		this.mDebugInfo.mColour = "#EBEBEB";
-		this.mDebugInfo.mString = "FPS (Framelimit): " + Math.round(nmain.game.mFPS) + " (" + nmain.game.mFrameLimit + ")";
+		this.mDebugInfo.mString = "FPS (Framelimit): " + Math.round(nmain.game.mFPS) + " (" + nmain.game.mFrameLimit + ")\n" +
+				"Map Seed: " + nmgrs.sceneMan.mCurrScene.mMap.mRand.GetSeed();
 		this.mDebugInfo.mDepth = -10000;
 		this.mDebugInfo.mShadow = true;
 		this.mDebugInfo.mPos.Set(camera.mTranslate.mX + 72, camera.mTranslate.mY + this.mDebugInfo.GetHeight());
 	}
 }
 
-GFGameUI.prototype.Input = function(camera) {
-	this.mTurnSprite.mPos.Set(camera.mTranslate.mX, camera.mTranslate.mY);
-	this.mArrowUpSprite.mPos.Set(camera.mTranslate.mX + nmain.game.mCanvasSize.mX - 46, camera.mTranslate.mY);
-	this.mArrowDownSprite.mPos.Set(camera.mTranslate.mX + nmain.game.mCanvasSize.mX - 46, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - 64);
+GFGameUI.prototype.Input = function() {
+	if (nmgrs.sceneMan.mCurrScene.mCanScroll == true) {
+		if (nmgrs.inputMan.GetMouseDown(nmouse.button.code.left)) {
+			// the mouse cursor position offset by the current camera (view)
+			var pt = new IVec2(0, 0);
+			pt.Copy(nmgrs.inputMan.GetLocalMouseCoords());
+			pt.mX += nmgrs.sceneMan.mCurrScene.mCam.mTranslate.mX; pt.mY += nmgrs.sceneMan.mCurrScene.mCam.mTranslate.mY;
+			
+			if (nmgrs.sceneMan.mCurrScene.mCam.mTranslate.mY > -24) {
+				// top left of the buttons boundbox
+				var tl = new IVec2(this.mArrowUpSprite.mPos.mX, this.mArrowUpSprite.mPos.mY);
+				
+				// bottom right of the buttons boundbox
+				var br = new IVec2(this.mArrowUpSprite.mPos.mX + this.mArrowUpSprite.GetWidth(),
+						this.mArrowUpSprite.mPos.mY + this.mArrowUpSprite.GetHeight());
+				
+				if (util.PointInRectangle(pt, tl, br) == true) {
+					nmgrs.sceneMan.mCurrScene.mCam.mTranslate.Set(nmgrs.sceneMan.mCurrScene.mCam.mTranslate.mX, nmgrs.sceneMan.mCurrScene.mCam.mTranslate.mY - 2);
+					
+					if (nmgrs.sceneMan.mCurrScene.mSelectID >= 0) {
+						nmgrs.sceneMan.mCurrScene.mGameEntities[nmgrs.sceneMan.mCurrScene.mSelectID].UpdateUI(nmgrs.sceneMan.mCurrScene.mCam);
+					}
+					
+					this.UpdateUI(nmgrs.sceneMan.mCurrScene.mCam);
+				}
+			}
+			
+			if (nmgrs.sceneMan.mCurrScene.mCam.mTranslate.mY + nmain.game.mCanvasSize.mY < (nmgrs.sceneMan.mCurrScene.mMap.mMapSize.mY * 32) + 24) {
+				// top left of the buttons boundbox
+				var tl = new IVec2(this.mArrowDownSprite.mPos.mX, this.mArrowDownSprite.mPos.mY);
+				
+				// bottom right of the buttons boundbox
+				var br = new IVec2(this.mArrowDownSprite.mPos.mX + this.mArrowDownSprite.GetWidth(),
+						this.mArrowDownSprite.mPos.mY + this.mArrowDownSprite.GetHeight());
+				
+				if (util.PointInRectangle(pt, tl, br) == true) {
+					nmgrs.sceneMan.mCurrScene.mCam.mTranslate.Set(nmgrs.sceneMan.mCurrScene.mCam.mTranslate.mX, nmgrs.sceneMan.mCurrScene.mCam.mTranslate.mY + 2);
+					
+					if (nmgrs.sceneMan.mCurrScene.mSelectID >= 0) {
+						nmgrs.sceneMan.mCurrScene.mGameEntities[nmgrs.sceneMan.mCurrScene.mSelectID].UpdateUI(nmgrs.sceneMan.mCurrScene.mCam);
+					}
+					
+					this.UpdateUI(nmgrs.sceneMan.mCurrScene.mCam);
+				}
+			}
+		}
+	}
 	
-	this.mControlsText.mPos.Set(camera.mTranslate.mX + 4, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - this.mControlsText.GetHeight() - 5);
-	
-	this.mEndTurnTapTextA.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextA.GetWidth() / 2), camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + 12);
-	this.mEndTurnTapTextB.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextB.GetWidth() / 2), camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + this.mEndTurnTapTextB.GetHeight() + 12);
-	
-	this.mDebugInfo.mPos.Set(camera.mTranslate.mX + 72, camera.mTranslate.mY + this.mDebugInfo.GetHeight());
+	if (nmgrs.inputMan.GetMousePressed(nmouse.button.code.left)) {
+		// top left of the buttons boundbox
+		var tl = new IVec2(this.mEndTurnSprite.mPos.mX, this.mEndTurnSprite.mPos.mY);
+		
+		// bottom right of the buttons boundbox
+		var br = new IVec2(this.mEndTurnSprite.mPos.mX + this.mEndTurnSprite.GetWidth(),
+				this.mEndTurnSprite.mPos.mY + this.mEndTurnSprite.GetHeight());
+		
+		if (util.PointInRectangle(pt, tl, br) == true) {
+			if (nmgrs.sceneMan.mCurrScene.mEndPlayerTurn == 0) {
+				nmgrs.sceneMan.mCurrScene.mEndPlayerTurn = 100;
+				this.mEndTurnSprite.SetCurrentFrame(0);
+				
+				if (nmgrs.sceneMan.mCurrScene.mSelectID >= 0) {
+					nmgrs.sceneMan.mCurrScene.mGameEntities[nmgrs.sceneMan.mCurrScene.mSelectID].SoftReset();
+				}
+			}
+			else {
+				nmgrs.sceneMan.mCurrScene.mEndPlayerTurn = 0;
+				this.mEndTurnSprite.SetCurrentFrame(1);
+				nmgrs.sceneMan.mCurrScene.mTurn = 2;
+				
+				this.SwitchTurn(1);
+			}
+		}
+	}
 }
 
 GFGameUI.prototype.Process = function() {
 	this.mArrowUpSprite.Process();
 	this.mArrowDownSprite.Process();
 	
-	this.mDebugInfo.mString = "FPS (Framelimit): " + Math.round(nmain.game.mFPS) + " (" + nmain.game.mFrameLimit + ")";
+	this.mEndTurnSprite.mPos.Set(nmgrs.sceneMan.mCurrScene.mCam.mTranslate.mX, nmgrs.sceneMan.mCurrScene.mCam.mTranslate.mY);
+	
+	this.mDebugInfo.mString = "FPS (Framelimit): " + Math.round(nmain.game.mFPS) + " (" + nmain.game.mFrameLimit + ")\n" +
+			"Map Seed: " + nmgrs.sceneMan.mCurrScene.mMap.mRand.GetSeed();
 }
 
 GFGameUI.prototype.Render = function(camera, turn, mapSize) {
@@ -105,7 +179,10 @@ GFGameUI.prototype.Render = function(camera, turn, mapSize) {
 		}
 	}
 	
-	this.mDynamicUIBatch.AddText(this.mControlsText);
+	// this.mDynamicUIBatch.AddText(this.mControlsText);
+	if (nmgrs.sceneMan.mCurrScene.mTurn == 1) {
+		this.mDynamicUIBatch.AddSprite(this.mEndTurnSprite);
+	}
 	
 	if (nmgrs.sceneMan.mCurrScene.mEndPlayerTurn > 0) {
 		this.mDynamicUIBatch.AddText(this.mEndTurnTapTextA);
@@ -117,6 +194,19 @@ GFGameUI.prototype.Render = function(camera, turn, mapSize) {
 	}
 	
 	this.mDynamicUIBatch.Render(camera);
+}
+
+GFGameUI.prototype.UpdateUI = function(camera) {
+	this.mTurnSprite.mPos.Set(camera.mTranslate.mX, camera.mTranslate.mY);
+	this.mArrowUpSprite.mPos.Set(camera.mTranslate.mX + nmain.game.mCanvasSize.mX - 64, camera.mTranslate.mY + 4);
+	this.mArrowDownSprite.mPos.Set(camera.mTranslate.mX + nmain.game.mCanvasSize.mX - 64, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - 68);
+	
+	this.mControlsText.mPos.Set(camera.mTranslate.mX + 4, camera.mTranslate.mY + nmain.game.mCanvasSize.mY - this.mControlsText.GetHeight() - 5);
+	
+	this.mEndTurnTapTextA.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextA.GetWidth() / 2), camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + 12);
+	this.mEndTurnTapTextB.mPos.Set(camera.mTranslate.mX + (nmain.game.mCanvasSize.mX / 2) - (this.mEndTurnTapTextB.GetWidth() / 2), camera.mTranslate.mY + this.mEndTurnTapTextA.GetHeight() + this.mEndTurnTapTextB.GetHeight() + 12);
+	
+	this.mDebugInfo.mPos.Set(camera.mTranslate.mX + 72, camera.mTranslate.mY + this.mDebugInfo.GetHeight());
 }
 
 GFGameUI.prototype.SwitchTurn = function(player) {
