644859e8ea669cfd854e13a5ca188366abcc4a0d
dakodun <jinto@chikin.net>
2012-12-01 10:34:31 +0000

    added logic to ready a scene without immdeiately switching which allows for interaction between scenes

--- parts/scenemanager.js
+++ parts/scenemanager.js
@@ -3,6 +3,9 @@
 function SceneManager() {
 	this.mCurrScene = null; // our current scene
 	this.mSceneStore = new Array(); // all of our stored (persistent) scenes
+	
+	this.mReadyScene = null; // the scene we will switch to set in ReadyScene()
+	this.mIsSetUp = false;
 }
 
 // initialises the scene manager
@@ -57,6 +60,54 @@ SceneManager.prototype.ChangeScene = function(newScene) {
 // returns the current scene
 SceneManager.prototype.GetCurrentScene = function() {
 	return this.mCurrScene;
+}
+
+// adds a new scene to the scene manager but doesn't yet switch which allows interaction betweens scenes
+SceneManager.prototype.ReadyScene = function(newScene) {
+	// this.mReadyScene = newScene;
+	var found = false; // indicates if we have found a previously stored scene
+	
+	// for all currently stored (persistent) scenes
+	for (var i = 0; i < this.mSceneStore.length; ++i) {
+		// if we find a match
+		if (this.mSceneStore[i].Type() == newScene.Type()) {
+			this.mReadyScene = this.mSceneStore[i]; // restore the stored scene as our ready (next) scene
+			this.mSceneStore.splice(i, i + 1); // remove it from the store
+			this.mIsSetUp = true;
+			found = true; // indicate we have found a persistent scene to restore
+			break;
+		}
+	}
+	
+	// if we didn't find a scene to restore
+	if (found == false) {
+		this.mReadyScene = newScene; // create a new scene
+		this.mIsSetUp = false;
+	}
+}
+
+// 
+SceneManager.prototype.SwitchScene = function() {
+	if (this.mReadyScene != null) {
+		// if we have a current scene (i.e., this is not our initial scene change on game start up)
+		if (this.mCurrScene != null) {
+			// if this scene is to be persistent
+			if (this.mCurrScene.Persistent() == true) {
+				this.mSceneStore.push(this.mCurrScene); // store this scene
+			}
+			else {
+				this.mCurrScene.TearDown(); // otherwise clean up and destroy this scene
+			}
+		}
+		
+		this.mCurrScene = this.mReadyScene;
+		
+		if (this.mIsSetUp == false) {
+			this.mCurrScene.SetUp();
+		}
+		
+		this.mReadyScene = null;
+	}
 }
 // ...End
 
